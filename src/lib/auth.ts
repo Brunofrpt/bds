2449eb7168e136564629b33827e7915146177992
Bruno <alvalade@live.fr>
2026-07-13 08:28:43 +0200

    brancher explicitement betther_auth_url et secret

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,24 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "@better-auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 
+const betterAuthUrl = process.env.BETTER_AUTH_URL;
+const betterAuthSecret = process.env.BETTER_AUTH_SECRET;
+
+if (!betterAuthUrl) {
+  throw new Error(
+    "La variable d'environnement BETTER_AUTH_URL est manquante.",
+  );
+}
+
+if (!betterAuthSecret) {
+  throw new Error(
+    "La variable d'environnement BETTER_AUTH_SECRET est manquante.",
+  );
+}
+
 export const auth = betterAuth({
+  baseURL: betterAuthUrl,
+  secret: betterAuthSecret,
   database: prismaAdapter(prisma, {
     provider: "mysql",
   }),
