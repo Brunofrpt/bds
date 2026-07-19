867228b17dad4d9749f09837f8c1b08e851f5756
Bruno <alvalade@live.fr>
2026-07-19 20:48:23 +0200

    limitations des workers next pour le build o2switch

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   experimental: {
+    cpus: 1,
+    workerThreads: false,
     serverActions: {
       bodySizeLimit: "8mb",
     },
